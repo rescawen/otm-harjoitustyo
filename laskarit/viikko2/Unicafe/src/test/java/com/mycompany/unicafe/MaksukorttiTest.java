e36fd46db635274685ace2ec8eea775667f2415d
Wenlei Dai <resca.wen@gmail.com>
2018-05-02 19:48:26 +0300

    jacoco 100%

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,38 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    @Test
+    public void saldoOlemassa() {
+        kortti.saldo();
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        System.out.println(kortti.toString());
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(2500);
+        assertEquals("saldo: 35.0", kortti.toString());
+    }
+    @Test
+    public void kortiltaVoiOttaaRahaa() {
+        kortti.otaRahaa(500);
+        assertEquals("saldo: 5.0", kortti.toString());
+        
+    }
+    @Test
+    public void kortiltaEiVoiOttaaLiikaaRahaa() {
+        kortti.otaRahaa(1500);
+        assertEquals("saldo: 10.0", kortti.toString());
+        
     }
 }
