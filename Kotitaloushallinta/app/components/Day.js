fbdf0861e83d963efd34498cfed6da196bb087ca
rescawen <resca.wen@gmail.com>
2018-06-26 13:58:37 +0300

    materializedcss styling lisätty

--- Kotitaloushallinta/app/components/Day.js
+++ Kotitaloushallinta/app/components/Day.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Day from './Day';
+
 
 export default class Home extends Component {
   static propTypes = {
-    onLogin: PropTypes.func.isRequired
+  
   };
   render() {
     return (
       <div>
-        <h2>Login</h2>
-        <input onChange={this.handleChange} type="text" value={this.state.username} />
-        <button onClick={this.handleLogin}>Log In</button>
+        
       </div>
     );
   }
