b3aa60ed097cf63fe5303cc51bb0ec3fb90e8070
rescawen <resca.wen@gmail.com>
2018-06-27 21:27:21 +0300

    testi lisätty

--- Kotitaloushallinta/test/actions/home.spec.js
+++ Kotitaloushallinta/test/actions/home.spec.js
@@ -9,20 +9,18 @@ describe('actions', () => {
 
   describe('home', () => {
     
-    it('should display task', () => {
+    it('should display task title', () => {
       const store = mockStore({});
       const expectedActions = [{
         type: 'DISPLAY_TASK',
         // WHAT SHOULD I PUT IN PAYLOAD?
         payload: {
-          username: 'John Doe',
-          loggedIn: true
+          title: 'Changing bedsheets',
         }
       }];
 
       store.dispatch(actions.displayTask({
-        username: 'John Doe',
-        loggedIn: true
+        title: 'Changing bedsheets',
       }));
 
       expect(store.getActions()).deep.equal(expectedActions);
