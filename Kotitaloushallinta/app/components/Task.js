17f58ceb7300401799c22a4eca470d35aac0befb
rescawen <resca.wen@gmail.com>
2018-06-27 16:17:17 +0300

    algorithm fixed millisecond bug

--- Kotitaloushallinta/app/components/Task.js
+++ Kotitaloushallinta/app/components/Task.js
@@ -9,18 +9,21 @@ export default class Task extends Component {
     render() {
 
         let timeRightNow = new Date().getTime();
+        console.log("time right now " + timeRightNow);
         let timeAtCreation = this.props.timeAtCreation;
-        let daysLeft = Math.floor((timeRightNow - timeAtCreation) / (60 * 60 * 24 * 60)) % this.props.days;
+        console.log("time at creation " + timeAtCreation);
+        console.log("difference in milliseconds " + (timeRightNow - timeAtCreation));
+        console.log("difference in days " + (timeRightNow - timeAtCreation)/ (1000 * 60 * 24 * 60));
+        console.log("difference in exact days " + Math.floor((timeRightNow - timeAtCreation) / (1000 * 60 * 24 * 60)));
+        let daysLeft = Math.floor((timeRightNow - timeAtCreation) / (1000 * 60 * 24 * 60)) % this.props.days;
 
-        console.log(daysLeft);
+        console.log("days left " + daysLeft);
 
         const daysList = [];
 
         for (let i = 0; i < this.props.days; i++) {
         // still need to work out frontend logic how does cycle end and start?
-            if (daysLeft == 0) {
-                daysList.push(<Day key={i} dayHasPassed='false' />)
-            } else if (i < this.props.days - daysLeft) {
+            if (i < daysLeft) {
                 daysList.push(<Day key={i} dayHasPassed='true' />)
             } else {
                 daysList.push(<Day key={i} dayHasPassed='false' />)
