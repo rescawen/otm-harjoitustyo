01341bcb6daa629848b0028ae67ac2b0fabe8659
rescawen <resca.wen@gmail.com>
2018-06-27 21:13:13 +0300

    kommentit selkeaksi

--- Kotitaloushallinta/app/components/Task.js
+++ Kotitaloushallinta/app/components/Task.js
@@ -9,20 +9,23 @@ export default class Task extends Component {
     render() {
 
         let timeRightNow = new Date().getTime();
-        console.log("time right now " + timeRightNow);
+        // ALL THE CONSOLE.LOG BELOW FOR TESTING ALGORITHM
+        // console.log("time right now " + timeRightNow);
         let timeAtCreation = this.props.timeAtCreation;
-        console.log("time at creation " + timeAtCreation);
-        console.log("difference in milliseconds " + (timeRightNow - timeAtCreation));
-        console.log("difference in days " + (timeRightNow - timeAtCreation)/ (1000 * 60 * 24 * 60));
-        console.log("difference in exact days " + Math.floor((timeRightNow - timeAtCreation) / (1000 * 60 * 24 * 60)));
+
+        // console.log("time at creation " + timeAtCreation);
+        // console.log("difference in milliseconds " + (timeRightNow - timeAtCreation));
+        // console.log("difference in days " + (timeRightNow - timeAtCreation)/ (1000 * 60 * 24 * 60));
+        // console.log("difference in exact days " + Math.floor((timeRightNow - timeAtCreation) / (1000 * 60 * 24 * 60)));
+
         let daysLeft = Math.floor((timeRightNow - timeAtCreation) / (1000 * 60 * 24 * 60)) % this.props.days;
 
-        console.log("days left " + daysLeft);
+        // console.log("days left " + daysLeft);
 
         const daysList = [];
 
         for (let i = 0; i < this.props.days; i++) {
-        // still need to work out frontend logic how does cycle end and start?
+        // right now it never fills all the blocks, always goes to filling 'totaldays - 1 blocks' and then all go blank
             if (i < daysLeft) {
                 daysList.push(<Day key={i} dayHasPassed='true' />)
             } else {
